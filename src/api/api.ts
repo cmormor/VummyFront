326e605d589ff468beb98296f0860648321e9227
cmormor <cmormor@g.educaand.es.com>
2025-05-26 22:23:40 +0200

    feat: api axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || "https://api.vummyapp.com/api/v1",
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "https://api.vummyapp.com/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
 });
 
 API.interceptors.request.use((config) => {
